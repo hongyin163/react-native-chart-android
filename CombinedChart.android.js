f11a6a3121308d146008ceccf43308b369bd5ccf
Jon <cnjon@users.noreply.github.com>
2016-01-16 10:17:38 +0800

    Update CombinedChart.android.js

--- CombinedChart.android.js
+++ CombinedChart.android.js
@@ -84,6 +84,20 @@ chart.propTypes = {
 };
 CombinedChart.Chart=chart;
 
+// RIGHT_OF_CHART,
+//  RIGHT_OF_CHART_CENTER,
+//  RIGHT_OF_CHART_INSIDE,
+//  LEFT_OF_CHART,
+//  LEFT_OF_CHART_CENTER,
+//  LEFT_OF_CHART_INSIDE,
+//  BELOW_CHART_LEFT,
+//  BELOW_CHART_RIGHT,
+//  BELOW_CHART_CENTER,
+//  ABOVE_CHART_LEFT,
+//  ABOVE_CHART_RIGHT,
+//  ABOVE_CHART_CENTER,
+//  PIECHART_CENTER;
+
 var MPCombinedChart = requireNativeComponent('MPCombinedChart', CombinedChart);
 
 export default CombinedChart;
