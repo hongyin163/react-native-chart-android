3b7bcfd74db0c57c07fec33bd44e0826f02f5fb9
Jon <cnjon@users.noreply.github.com>
2016-04-15 14:52:16 +0800

    Update MPPieRadarChartManager.java

--- android/src/main/java/cn/mandata/react_native_mpchart/MPPieRadarChartManager.java
+++ android/src/main/java/cn/mandata/react_native_mpchart/MPPieRadarChartManager.java
@@ -4,23 +4,15 @@ import android.graphics.Color;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.charts.PieRadarChartBase;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieRadarChartBase;
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.LimitLine;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -38,9 +30,9 @@ public class MPPieRadarChartManager extends SimpleViewManager<PieRadarChartBase>
 
     @Override
     protected PieRadarChartBase createViewInstance(ThemedReactContext reactContext) {
-        return null;
+        PieChart chart = new PieChart(reactContext);
+        return chart;
     }
-
 
     @ReactProp(name="touchEnabled",defaultBoolean = true)
     public void setTouchEnabled(PieRadarChartBase chart,boolean enable){
