7f176bdace17581a916f2fbe74723f75498d34ac
Silvio Rainoldi <ianaz90@gmail.com>
2017-03-13 08:42:53 +0100

    Fix for newer React native versions

--- android/src/main/java/cn/mandata/react_native_mpchart/MPCombinedChartManager.java
+++ android/src/main/java/cn/mandata/react_native_mpchart/MPCombinedChartManager.java
@@ -7,7 +7,7 @@ import android.graphics.Paint;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.ReactPropGroup;
+import com.facebook.react.uimanager.annotations.ReactPropGroup;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
