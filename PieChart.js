4ef49135b63d868bfd243ff8c1ec092dde55d6bb
William <wkwyatt@users.noreply.github.com>
2018-03-12 15:41:22 -0400

    Replace deprecated PropTypes from React
    - update prop types

--- PieChart.js
+++ PieChart.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {requireNativeComponent, View} from 'react-native';
 
 class PieChart extends Component {
